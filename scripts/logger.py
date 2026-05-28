aa50526a2b5d839f60dead204ddb43daf8de7448
zeroG7-cmd <zerogravity845@gmail.com>
2026-05-28 04:59:35 +0100

    Add documentation header to logger.py
    Added header comments to logger.py for documentation.

---
+++ scripts/logger.py
@@ -0,0 +1,25 @@
+# --------------------------------------------------
+# Shadow Robotics Logger System
+#
+# Author: Sidyfon Afangide
+# Alias : Zer0
+#
+# Project:
+# Shadow Drone R&D Environment
+#
+# Purpose:
+# Logging and managing robotics test data
+# for simulation and hardware workflows.
+# --------------------------------------------------
+
+# -----------------------------
+# Imports
+# -----------------------------
+import os
+import sqlite3
+from datetime import datetime
+
+# -----------------------------
+# Paths / Configuration
+# -----------------------------
+
